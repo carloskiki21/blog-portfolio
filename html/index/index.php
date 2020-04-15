d26113df5d48b6e7300f3cf6493809741e4e3c01
Carlos <carlosegonzax@gmail.com>
2020-04-15 14:23:35 -0400

    MVC implementation

---
+++ html/index/index.php
@@ -0,0 +1,104 @@
+<?php include(HTML_DIR . 'overall/header.php'); ?>
+
+<?php include(HTML_DIR . 'overall/topnav.php'); ?>
+
+<!-- Main section -->
+<main role="main" class="container">
+    <br /> <br />
+
+
+    <!-- Main article -->
+    <div class="row">
+      <div class="col-md-8">
+        <img class="img-responsive" width="100%" src="http://via.placeholder.com/750x500" alt="">
+      </div>
+      <div class="col-md-4">
+        <br />
+        <span class="main-article-category">Article category</span>
+        <br />
+        <span class="main-article-tittle">This is the article tittle.. Crazy thing are happening</span>
+        <br />
+        <span>By <span class="autor-name">Kiki</span></span>
+      </div>
+    </div>
+
+    <!-- Sub-section -->
+    <br /> <br />
+    <span class="recent-articles">Recent articles</span>
+    <hr />
+
+    <div class="row">
+
+      <!-- Recents articles -->
+      <div class="col-md-8">
+
+        <!-- Recent Article design -->
+        <div class="row">
+          <div class="col-md-4">
+            <img class="img-responsive" width="100%" src="http://via.placeholder.com/750x500" alt="">
+          </div>
+          <div class="col-md-8">
+            <br />
+            <span class="sub-article-date">April, 14, 2020</span>
+            <br />
+            <span class="sub-article-tittle">This is the tittle of the first sub-article</span>
+            <br />
+            <span class="sub-article-author">By <span class="author-name">Kiki</span></span>
+            <br />
+            <span class="sub-article-category">Article category</span>
+          </div>
+        </div>
+        <hr />
+
+        <!-- Recent Article design -->
+        <div class="row">
+          <div class="col-md-4">
+            <img class="img-responsive" width="100%" src="http://via.placeholder.com/750x500" alt="">
+          </div>
+          <div class="col-md-8">
+            <br />
+            <span class="sub-article-date">April, 15, 2020</span>
+            <br />
+            <span class="sub-article-tittle">DAAAAM! 500 more deaths</span>
+            <br />
+            <span class="sub-article-author">By <span class="author-name">Sam</span></span>
+            <br />
+            <span class="sub-article-category">Article category</span>
+          </div>
+        </div>
+        <hr />
+
+        <!-- Recent Article design -->
+        <div class="row">
+          <div class="col-md-4">
+            <img class="img-responsive" width="100%" src="http://via.placeholder.com/750x500" alt="">
+          </div>
+          <div class="col-md-8">
+            <br />
+            <span class="sub-article-date">April, 16, 2020</span>
+            <br />
+            <span class="sub-article-tittle">There we go boyz, we diying</span>
+            <br />
+            <span class="sub-article-author">By <span class="author-name">Kiki</span></span>
+            <br />
+            <span class="sub-article-category">Article category</span>
+          </div>
+        </div>
+        <hr />
+
+        <!-- More tag -->
+        <div class="text-right"><span class="demo-link">More...</span></div>
+
+      </div>
+
+      <!-- Sponsors add -->
+      <div class="col-md-4">
+        <span class="sponsor">SPONSOR</span>
+        <br /><br />
+        <img class="img-responsive" width="100%" src="http://via.placeholder.com/600x800" alt="">
+      </div>
+
+    </div>
+  </main>
+
+  <?php include(HTML_DIR . 'overall/footer.php'); ?>
---
+++ html/overall/topnav.php
@@ -0,0 +1,36 @@
+<section>
+
+<!-- <img class="img-responsive" width="100%" src="http://via.placeholder.com/1300x200" alt=""> -->
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+        <a class="navbar-brand" href="?view=index">Blog</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#zika" aria-controls="zika" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+        <div class="collapse navbar-collapse" id="zika">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="applesauce" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Code</a>
+                    <div class="dropdown-menu" aria-labelledby="applesauce">
+                        <a href="#" class="dropdown-item">Web dev</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="choco" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Art</a>
+                    <div class="dropdown-menu" aria-labelledby="choco">
+                        <a href="#" class="dropdown-item">Draw</a>
+                    </div>
+                </li>
+            </ul>
+
+            <form class="form-inline my-2 my-lg-0" action="">
+                <input type="text" placeholder="Search" class="form-control mr-sm-2">
+                <button type="button" class="btn btn-outline-success my-2 my-sm" value="">Submit</button>
+            </form>
+        </div>
+    </nav>
+	
+</section>
